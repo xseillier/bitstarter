e3ef0794a80102df88f2049a25fd57eaa88bae99
Seillier Xavier <xseillier@gmail.com>
2013-07-07 01:24:35 +0200

    mise a jours

--- grader.js
+++ grader.js
@@ -24,6 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest    = require('restler');
+
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,24 +39,46 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+
+var checkHtmlUrl = function( urlfile, checksfile ) {
+    
+	return rest.get( urlfile ).on('complete', function( data )
+	{
+		checkFile( cheerio.load( data ), checksfile );
+	});
 };
+
+
+var checkHtmlFile = function( pathfile, checksfile ) {
+
+	fs.readFile( pathfile , function (err, data) {
+	  if (err) throw err;
+	  checkFile( cheerio.load( data ), checksfile );
+	});
+};
+
+
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+
+var checkFile = function(flux, checksfile) {
+    $ = flux;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
 };
+
+
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -64,11 +89,19 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file [html_file]', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url [ur]', 'Url to index.html' )
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    
+    if( program.url != undefined )
+    {
+	checkHtmlUrl(  program.url, program.checks );
+    }
+    else 
+    {
+	checkHtmlFile( program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
